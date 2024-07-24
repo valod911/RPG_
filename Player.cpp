da8c48dd029f9a589d409a43387094311bbb6807
valod911 <wowan92bbb@gmail.com>
2024-07-24 21:23:29 +0200

    motion animation works in all directions

--- Player.cpp
+++ Player.cpp
@@ -24,8 +24,8 @@ Player::Player(float x, float y,sf::Texture& texture_sheet)
 	this->createMovementComponent(100.f, 5.f, 3.f);
 	this->createAnimationComponent(texture_sheet);
 
-	this->animationComponent->addAnimation("IDLE_RIGHT", 10.f, 0, 0, 6, 0, 128, 128);
-	this->animationComponent->addAnimation("WALK_RIGHT", 10.f, 0, 1, 6, 1, 128, 128);
+	this->animationComponent->addAnimation("IDLE", 10.f, 0, 0, 6, 0, 128, 128);
+	this->animationComponent->addAnimation("WALK", 10.f, 0, 1, 6, 1, 128, 128);
 }
 
 Player::~Player()
@@ -37,9 +37,27 @@ void Player::update(const float& dt)
 {
 	this->movementComponent->update(dt);
 	if (this->movementComponent->getState(IDLE))
-		this->animationComponent->play("IDLE_RIGHT", dt);
+		this->animationComponent->play("IDLE", dt);
 	else if(this->movementComponent->getState(MOVING_LEFT))
-		this->animationComponent->play("WALK_RIGHT", dt);
+	{
+		this->sprite.setOrigin(129.f, 0.f);
+		this->sprite.setScale(-1.f, 1.f);
+		this->animationComponent->play("WALK", dt);
+	}
+	else if (this->movementComponent->getState(MOVING_RIGHT))
+	{
+		this->sprite.setOrigin(0.f, 0.f);
+		this->sprite.setScale(1.f, 1.f);
+		this->animationComponent->play("WALK", dt);
+	}
+	else if (this->movementComponent->getState(MOVING_UP))
+	{
+		this->animationComponent->play("WALK", dt);
+	}
+	else if (this->movementComponent->getState(MOVING_DOWN))
+	{
+		this->animationComponent->play("WALK", dt);
+	}
 
 	this->hitboxComponent->update();
 }
